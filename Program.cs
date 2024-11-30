f13a328f9acf8e4ca5fb2cb12afd6deaf127175c
valentinf <azubalapk@proton.me>
2024-11-30 15:16:26 +0100

    update dependency

--- Program.cs
+++ Program.cs
@@ -36,27 +36,6 @@ app.UseHttpsRedirection();
 // Utiliser CORS
 app.UseCors("AllowAll");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-
-    return Results.Ok(forecast);
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 
 app.MapGet("/gameparts",() => {
     var gameparts = db.GameParts;
@@ -91,9 +70,4 @@ app.MapPost("/gameparts", async (HttpRequest request) =>
 
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
